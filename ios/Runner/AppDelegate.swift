97ded3855d76a260e8246e39b9705400b187c680
Maximilian Fuchs <maxifuchs98@gmail.com>
2021-03-20 18:03:02 +0100

    Fixes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import MapKit
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -11,5 +12,36 @@ import Flutter
     registrar(forPlugin: "testapp")!.register(mapFactory, withId: "MapView")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
+
+class MapFactory: NSObject, FlutterPlatformViewFactory {
+    func create(withFrame frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?) -> FlutterPlatformView {
+        return MapView(frame, viewId: viewId, args: args)
+    }
+}
+
+class MapView: NSObject, FlutterPlatformView {
+    let frame: CGRect
+    let viewId: Int64
+
+    init(_ frame: CGRect, viewId: Int64, args: Any?) {
+        self.frame = frame
+        self.viewId = viewId
+    }
+
+    func view() -> UIView {
+        return MKMapView(frame: frame)
+    }
+}
+
+public class MapPlugin: NSObject, FlutterPlugin {
+  public static func register(with registrar: FlutterPluginRegistrar) {
+    let viewFactory = MapFactory()
+    registrar.register(viewFactory, withId: "testapp/maps")
+  }
+
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    result("iOS " + UIDevice.current.systemVersion)
   }
 }
